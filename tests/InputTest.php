53e24348a9d2d737cb9090621ae7ae4d45473052
joscul <josefcullhed@gmail.com>
2021-09-11 14:12:19 +0200

    Added more tests to calculate_offsets

--- tests/InputTest.php
+++ tests/InputTest.php
@@ -37,8 +37,17 @@ final class InputTest extends TestCase {
 	public function test_calculate_offsets() {
 		$current_page = 1;
 		$results_per_page = 10;
+
 		list($offset_start, $offset_end) = calculate_offsets($current_page, $results_per_page);
 		$this->assertEquals($offset_start, 0);
+		$this->assertEquals($offset_end, 10);
+
+		list($offset_start, $offset_end) = calculate_offsets(2, $results_per_page);
+		$this->assertEquals($offset_start, 10);
+		$this->assertEquals($offset_end, 20);
+
+		list($offset_start, $offset_end) = calculate_offsets(2, 5);
+		$this->assertEquals($offset_start, 5);
 		$this->assertEquals($offset_end, 10);
 	}
 
